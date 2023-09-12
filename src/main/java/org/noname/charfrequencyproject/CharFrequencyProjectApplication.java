617e77508718ab72678ee0efbeeea4de7f1f63c4
hulio13 <andrej.tsyn@yandex.ru>
2023-09-12 19:25:05 +0300

    Initial commit

---
+++ src/main/java/org/noname/charfrequencyproject/CharFrequencyProjectApplication.java
@@ -0,0 +1,13 @@
+package org.noname.charfrequencyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharFrequencyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharFrequencyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/noname/charfrequencyproject/CharFrequencyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.noname.charfrequencyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharFrequencyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

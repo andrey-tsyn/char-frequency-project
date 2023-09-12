3e75611db8e3bbea1866ff1f7efaef638813da3d
hulio13 <andrej.tsyn@yandex.ru>
2023-09-12 20:01:50 +0300

    feat(entity): add ok and error static methods to Result

--- src/main/java/org/noname/charfrequencyproject/entity/common/Result.java
+++ src/main/java/org/noname/charfrequencyproject/entity/common/Result.java
@@ -1,4 +1,11 @@
 package org.noname.charfrequencyproject.entity.common;
 
 public record Result<T>(boolean isSuccess, T result, Error error) {
+    public static <T> Result<T> ok(T result) {
+        return new Result<>(true, result, null);
+    }
+
+    public static <T> Result<T> error(Error error) {
+        return new Result<>(false, null, error);
+    }
 }

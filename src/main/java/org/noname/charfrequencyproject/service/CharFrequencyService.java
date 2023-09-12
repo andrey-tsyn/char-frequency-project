554213e15f247093a47df485ea4ed85b8c6c3d13
hulio13 <andrej.tsyn@yandex.ru>
2023-09-12 19:47:39 +0300

    feat(service): create CharFrequencyService with getCharFrequencyFromString method

---
+++ src/main/java/org/noname/charfrequencyproject/service/CharFrequencyService.java
@@ -0,0 +1,23 @@
+package org.noname.charfrequencyproject.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class CharFrequencyService {
+
+    public HashMap<Character, Integer> getCharFrequencyFromString(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (Character ch : str.toCharArray()) {
+            if (!map.containsKey(ch)) {
+                map.put(ch, 1);
+            } else {
+                map.put(ch, map.get(ch) + 1);
+            }
+        }
+
+        return map;
+    }
+}

5d00d57b92226519ecb9a4fc7e895f08a64ba5f9
hulio13 <andrej.tsyn@yandex.ru>
2023-09-13 22:07:04 +0300

    fix(web): add path in RequestMapping instead of RestController

--- src/main/java/org/noname/charfrequencyproject/web/controller/CharFrequencyController.java
+++ src/main/java/org/noname/charfrequencyproject/web/controller/CharFrequencyController.java
@@ -1,9 +1,21 @@
 package org.noname.charfrequencyproject.web.controller;
 
+import org.noname.charfrequencyproject.entity.common.Result;
 import org.noname.charfrequencyproject.service.CharFrequencyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/charFrequency")
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/charFrequency")
 public class CharFrequencyController {
     private final CharFrequencyService frequencyService;
 
